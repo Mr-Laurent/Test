4bcd0a8dceec2b6175a94a134f53f59c493f45d9
Mr-Laurent <39402868+Mr-Laurent@users.noreply.github.com>
2023-12-01 17:29:32 +0100

    Add files via upload

---
+++ TheCode.R
@@ -0,0 +1,24 @@
+
+
+
+|               *                      *
+|                            *                *
+|
+|          *        *               *        
+|                        ???
+|                        ""
+|             *         "VV"              *
+|                     "V"V"VV"
+|                *   0  "VV"  0
+|                     "VVVVVV"                *
+|           *      "VVVVVVV"V"VVV"
+|                  0   "V"V"V"   0    *
+|                   "VV"V"VVVVV"
+|               "VVVV"V"VVVVVVVVVVV"
+|               0     "V"V"VV"     0
+|         *        "VVVVVVV"V"VV"
+|               "VVVVVVVVVVVV"V"VVV"
+|            "VVVVVVVVVVVVVVVVVV"V"VVV"    *
+|            0      0   |||   0       0
+|                       |||
+|        ####################################    
